7300dfe2fc0fdf52ce73718dd6b5f4e30f08afc2
arihaantodi <arihaantodi@gmail.com>
2024-07-19 18:25:00 +0530

    portfolio

---
+++ Bulb/bulb.js
@@ -0,0 +1,9 @@
+function turnOff() {
+  document.querySelector("h1").style.color = "grey";
+  document.querySelector("img").src = "dark bulb.png";
+}
+
+function turnOn() {
+  document.querySelector("h1").style.color = "orangered";
+  document.querySelector("img").src = "glowing bulb.png";
+}
