328c0b9e9ee1a78df7ce3dfa5d8f57f66a03600f
arihaantodi <arihaantodi@gmail.com>
2024-07-26 18:07:25 +0530

    Addition of Button Project

---
+++ Button/button.js
@@ -0,0 +1,5 @@
+function changeColor() {
+  document.querySelector("body").style.backgroundColor = "indianred";
+  document.querySelector("h2").style.color = "white";
+  document.querySelector("h4").style.color = "white";
+}
