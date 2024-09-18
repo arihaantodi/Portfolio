48052b3fb346d9cf538e98d227e47b75a80eb6b9
arihaantodi <arihaantodi@gmail.com>
2024-09-18 18:02:11 +0530

    qwerty

---
+++ Hobbies/hobbies.js
@@ -0,0 +1,8 @@
+function text(event) {
+  document.querySelector(".right-side").style.color = event.target.value;
+}
+
+function bg(event) {
+  document.querySelector(".right-side").style.backgroundColor =
+    event.target.value;
+}
